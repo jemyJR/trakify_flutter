723371e4fef5b9f4cef2fa6a42ebc5ec4aaaadef
jemyJR <mmohamedabdelrahman@student.eelu.edu.eg>
2025-03-28 17:30:41 +0200

    - Added app helpers - Added cache helper

---
+++ lib/core/theming/app_styles.dart
@@ -0,0 +1,26 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
+import 'package:trakify/core/theming/app_colors.dart';
+
+class AppStyles {
+  static TextStyle font20w600Black = TextStyle(
+    fontSize: 20.sp,
+    fontWeight: FontWeight.w600,
+    color: AppColors.black,
+  );
+  static TextStyle font16w500Black = TextStyle(
+    fontSize: 16.sp,
+    fontWeight: FontWeight.w500,
+    color: AppColors.black,
+  );
+  static TextStyle font16w600White = TextStyle(
+    fontSize: 16.sp,
+    fontWeight: FontWeight.w600,
+    color: AppColors.white,
+  );
+  static TextStyle font16w600Primary = TextStyle(
+    fontSize: 16.sp,
+    fontWeight: FontWeight.w600,
+    color: AppColors.primary,
+  );
+}
